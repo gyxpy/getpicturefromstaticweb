ce47c19e620f59d57e0673990d271c1549aa0bae
gyxpy <18801468753@163.com>
2017-09-01 21:58:51 +0800

    完成
    可以达到要求了。爬取静态网站并储存。不过路径要先设置成默认打的

--- getpic.py
+++ getpic.py
@@ -12,6 +12,12 @@ import re
 import requests
 import os
 GS=['.jpg','.png']
+
+class MyException(Exception):#异常处理
+    def __init__(self,message):
+        Exception.__init__(self)
+        self.message=message
+
 def gethtmltxt(url):#获取页面的txt
     try:
         r=requests.get(url)
@@ -20,67 +26,55 @@ def gethtmltxt(url):#获取页面的txt
         return r.text
     except:
         return ''
-def getpic(url,name,path):
+
+def getpic(url,name):#获得图片并保存，请事先设置路径
     try:
         r=requests.get(url)
         r.raise_for_status()
     except:
         return ''
-    os.chdir(path)#转到路径
     #获得格式
-    pic_cls=re.findall(r'\.\w{2,4}',url)[0]
+    pic_cls=re.findall(r'\.\w{2,4}',url)[-1]
     if pic_cls in GS:
         name=name+pic_cls#构建完整名称并写入
         with open(name,'wb') as f:
             f.write(r.content)
     else:
         return ''
-'''    
-def mkdir(path):
-    # 去除首位空格
-    path=path.strip()
-    # 去除尾部 \ 符号
-    path=path.rstrip("\\")
- 
-    # 判断路径是否存在
-    # 存在     True
-    # 不存在   False
-    isExists=os.path.exists(path)
- 
-    # 判断结果
-    if not isExists:
-        # 如果不存在则创建目录
-        # 创建目录操作函数
-        os.makedirs(path) 
- 
-        print(path+' 创建成功')
-        return True
-    else:
-        # 如果目录存在则不创建，并提示目录已存在
-        print(path+' 目录已存在')
-        return False
-'''
-def mkdir(path):
+
+def mkdir(path):#创建目录
     if not os.path.exists(path):
         os.makedirs(path)
 
-#输入页面的URL链接与存储URL链接的列表(页面的链接，图片链接列表，需要的图片格式jpg?)
-def getpicurl(url,ult,jpblist):
-    for j in jpglist:
+def ifOKjpblist(jpblist):#判断输入的图片类型是否符合要求
+    for i in jpblist:
+        j='.'+i
         if j in GS:
             continue
         else:
-            print(wrong)
-    rx=re.compile(r'http。+?')#构建匹配图片URL链接的正则表达式
-    ult+=re.findall(rx,gethtmltxt(url))
+            raise MyException('Not in list')
+#获得一个页面的所有图片链接
+#(页面的链接，图片链接列表，需要的图片格式)
+def getpicurl(url,jpblist):
+    ult=[]
+    ifOKjpblist(jpblist)
+    for mt in jpblist:
+        rx=re.compile(r'http.+?'+mt)
+        ult+=re.findall(rx,gethtmltxt(url))
+    return ult
+
 def main():
-    path=''
+    path='C:\\Users\\lenovo\\Desktop\\临时文件\\bgirl\\Newwwww'
+    rooturl='https://www.bbb960.com/htm/pic8/132034.htm'
     mkdir(path)
     os.chdir(path)
-    ult=[]
-    getpicurl(ult)
+    ult=getpicurl(rooturl,['jpg'])
+    count=1
     for url in ult:
-        
+        getpic(url,str(count))
+        count+=1
+    #getpic('https://img.581gg.com/picdata-watermark/a1/247/24792-8.jpg','1')
+    #print('OK')
         
     
     
