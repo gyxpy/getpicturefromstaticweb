96ddef2cd9378d4abc3d1d0a9d624f8967e9719f
gyxpy <18801468753@163.com>
2017-09-02 15:44:57 +0800

    多线程及界面开始

--- getthreading.py
+++ getthreading.py
@@ -7,3 +7,8 @@ Created on Fri Sep  1 22:05:47 2017
 # =============================================================================
 # 并行爬取图片
 # =============================================================================
+import re
+import requests
+from user_agent import user_agent_dict as user
+import os
+from 
